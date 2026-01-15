e417dcf7e237f5fcb7154052c9ad018ca39a720d
Hayeon Baek <bhy0512@gmail.com>
2026-01-15 16:49:08 +0900

    feat: 로그인 여부에 따라 페이지 접근 제한

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { Navigate, Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import PublicRoute from './components/auth/PublicRoute';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Route element={<PublicRoute />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
